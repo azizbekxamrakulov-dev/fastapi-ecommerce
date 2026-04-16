49e3e38754d9e4c46a0c210e4b90056e616e2cbc
azizbekxamrakulov-dev <azizbek.xamrokulov@gmail.com>
2026-04-16 17:22:05 +0500

    cod

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+
 # test
 from app.db.session import get_db
 
 
+
 app = FastAPI(title="Ecommerce")
+
 
 session = next(get_db())
 print(session.connection())
--- run.py
+++ run.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app", host=settings.host, port=settings.port, reload=settings.debug
     )
+
+()
